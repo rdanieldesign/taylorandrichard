868934f2d5cfa2ec97e2c4f9bad5d4de30bb798b
rdanieldesign <rdanieldesign@gmail.com>
2015-07-29 20:21:03 -0400

    Add scrolling animation for anchors

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,27 @@
-$(function() {
-	$('a[href*=#]:not([href=#])').click(function() {
-		if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-			if (target.length) {
+'use strict';
+
+var TAR = {
+
+	init: function(){
+		this.scrollAnimation();
+	},
+
+	scrollAnimation: function(){
+		$('a[href*=#]:not([href=#])').click(function(e) {
+			e.preventDefault();
+			if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+				var hash = this.hash.slice(1);
+				var target = document.getElementById(hash);
 				$('.content').animate({
-					scrollTop: target.offset().top
+					scrollTop: target.offsetTop
 				}, 1000);
 				return false;
 			}
-		}
-	});
+		});
+	}
+
+};
+
+$(function() {
+	TAR.init();
 });
